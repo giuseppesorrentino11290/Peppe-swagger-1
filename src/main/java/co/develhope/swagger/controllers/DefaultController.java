0c91edcab80c942ee08bde362c68c5c1cbb03067
giuseppe sorrentino <giuseppesorrentino11290@gmail.com>
2023-01-23 17:14:06 +0100

    Initial commit

---
+++ src/main/java/co/develhope/swagger/controllers/DefaultController.java
@@ -0,0 +1,14 @@
+package co.develhope.swagger.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "Ciaoneeeee ";
+    }
+}
---
+++ src/main/java/co/develhope/swagger/controllers/NameController.java
@@ -0,0 +1,17 @@
+package co.develhope.swagger.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/name")
+public class NameController {
+
+    @GetMapping
+    public String getName(){
+        return " Giuseppe ";
+
+    }
+}
